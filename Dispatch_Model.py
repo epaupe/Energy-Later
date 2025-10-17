57c5c8924a068723037995753d443a4fb76daf63
Thijs de Boer <thijsantonie2002@gmail.com>
2025-10-17 10:21:53 +0200

    dec vars

--- Dispatch_Model.py
+++ Dispatch_Model.py
@@ -2,7 +2,18 @@ import numpy as np
 import gurobipy as gp
 from gurobipy import GRB
 from gurobipy import quicksum
+
 #Decision Variables:
 
-
 #Constraints:
+
+#Initialize optimization problem:
+MMO = gp.Model("Multi Market Optimization")
+
+days = 365
+total_hours = days * 24
+Horizon = range(total_hours + 1)
+
+Charge = MMO.addVars(Horizon, vtype=GRB.CONTINUOUS, lb=0, ub=10 name="Battery_Charge [MW]") 
+Discharge = MMO.addVars(Horizon, vtype=GRB.CONTINUOUS, lb=0, ub=10, name="Battery_Discharge [MW]")
+SOC = MMO.addVars(Horizon, vtype=GRB.CONTINUOUS, lb=0, ub=40, name="Battery_Discharge [MWh]") 
